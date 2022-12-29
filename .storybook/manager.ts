520de4f83aed7fa7e097dfad1bf33227b9706957
AnhMTV <maithachvietanh@gmail.com>
2022-12-29 09:52:47 +0700

    [Issue-11] Update logo and favicon.ico for theme previewer

--- .storybook/manager.ts
+++ .storybook/manager.ts
@@ -1,6 +1,10 @@
 import { addons } from '@storybook/addons';
 import { themes } from '@storybook/theming';
+const logo = require('./subwallet.png');
+
+const theme = themes.dark;
+theme.brandImage = logo;
 
 addons.setConfig({
-  theme: themes.dark,
+  theme,
 });
