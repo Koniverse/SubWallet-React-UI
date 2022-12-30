928218f40cdf11075389cd23f95c672bb1c5ddb0
dominhquang <quangdm@subwallet.app>
2022-12-30 09:33:18 +0700

    [Issue-26] Update Image

--- components/image/index.tsx
+++ components/image/index.tsx
@@ -1,63 +1,60 @@
-import EyeOutlined from '@ant-design/icons/EyeOutlined';
 import classNames from 'classnames';
 import RcImage, { type ImageProps } from 'rc-image';
 import * as React from 'react';
+import Squircle from '../squircle/index';
 import { ConfigContext } from '../config-provider';
-import defaultLocale from '../locale/en_US';
-import { getTransitionName } from '../_util/motion';
 // CSSINJS
-import PreviewGroup, { icons } from './PreviewGroup';
 import useStyle from './style';
 
-export interface CompositionImage<P> extends React.FC<P> {
-  PreviewGroup: typeof PreviewGroup;
+const ImageShapes = ['default', 'square', 'rounded', 'circle', 'squircle'] as const;
+export type ImageShape = typeof ImageShapes[number];
+
+export interface SwImageProps extends ImageProps {
+  shape?: ImageShape;
+  width: string;
+  height?: string;
+  responsive?: boolean;
 }
 
-const Image: CompositionImage<ImageProps> = ({
+const Image: React.FC<SwImageProps> = ({
   prefixCls: customizePrefixCls,
   preview,
   rootClassName,
+  shape = 'default',
+  width,
+  height = 'auto',
+  responsive,
   ...otherProps
 }) => {
-  const {
-    getPrefixCls,
-    locale: contextLocale = defaultLocale,
-    getPopupContainer: getContextPopupContainer,
-  } = React.useContext(ConfigContext);
+  const { getPrefixCls } = React.useContext(ConfigContext);
 
   const prefixCls = getPrefixCls('image', customizePrefixCls);
-  const rootPrefixCls = getPrefixCls();
-
-  const imageLocale = contextLocale.Image || defaultLocale.Image;
   // Style
   const [wrapSSR, hashId] = useStyle(prefixCls);
 
-  const mergedRootClassName = classNames(rootClassName, hashId);
-  const mergedPreview = React.useMemo(() => {
-    if (preview === false) {
-      return preview;
-    }
-    const _preview = typeof preview === 'object' ? preview : {};
-    const { getContainer, ...restPreviewProps } = _preview;
-    return {
-      mask: (
-        <div className={`${prefixCls}-mask-info`}>
-          <EyeOutlined />
-          {imageLocale?.preview}
-        </div>
-      ),
-      icons,
-      ...restPreviewProps,
-      getContainer: getContainer || getContextPopupContainer,
-      transitionName: getTransitionName(rootPrefixCls, 'zoom', _preview.transitionName),
-      maskTransitionName: getTransitionName(rootPrefixCls, 'fade', _preview.maskTransitionName),
-    };
-  }, [preview, imageLocale]);
+  const mergedRootClassName = classNames(rootClassName, hashId, {
+    [`-shape-${shape}`]: shape !== 'default' && shape,
+    '-responsive': !!responsive,
+  });
+  if (shape === 'squircle') {
+    return wrapSSR(
+      <Squircle customSize={width}>
+        <RcImage
+          style={{ width, height }}
+          preview={false}
+          prefixCls={`${prefixCls}`}
+          rootClassName={mergedRootClassName}
+          {...otherProps}
+        />
+      </Squircle>,
+    );
+  }
 
   return wrapSSR(
     <RcImage
+      style={{ width, height: shape === 'circle' ? width : height }}
+      preview={false}
       prefixCls={`${prefixCls}`}
-      preview={mergedPreview}
       rootClassName={mergedRootClassName}
       {...otherProps}
     />,
@@ -65,7 +62,5 @@ const Image: CompositionImage<ImageProps> = ({
 };
 
 export { ImageProps };
-
-Image.PreviewGroup = PreviewGroup;
 
 export default Image;
---
+++ components/image/stories/image.stories.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
+import Image from '..';
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Core/Image',
+  component: Image,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    shape: {
+      control: 'radio',
+      options: ['default', 'square', 'rounded', 'circle', 'squircle'],
+      defaultValue: 'default',
+    },
+    width: { control: 'text' },
+    height: { control: 'text', if: { arg: 'height', truthy: true } },
+    responsive: { control: 'boolean', defaultValue: true },
+  },
+} as ComponentMeta<typeof Image>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Image> = (args) => <Image {...args} />;
+
+const imgLink = 'https://via.placeholder.com/200/004BFF';
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  src: imgLink,
+  width: '200px',
+  height: '200px',
+};
+
+export const Square = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Square.args = {
+  src: imgLink,
+  shape: 'square',
+  width: '200px',
+};
+
+export const Rounded = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Rounded.args = {
+  src: imgLink,
+  shape: 'rounded',
+  width: '200px',
+  height: '200px',
+};
+
+export const Circle = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Circle.args = {
+  src: imgLink,
+  shape: 'circle',
+  width: '200px',
+};
+
+export const Squircle = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Squircle.args = {
+  src: imgLink,
+  width: '200px',
+  shape: 'squircle',
+};
