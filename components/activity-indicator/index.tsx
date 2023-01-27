4b6d7ff6e6f15266c479cd93c338e74b4de6a5e6
dominhquang <quangdm@subwallet.app>
2023-01-27 15:09:21 +0700

    [Issue-37] Update Activity Indicator

--- components/activity-indicator/index.tsx
+++ components/activity-indicator/index.tsx
@@ -1,132 +1,62 @@
-import classNames from 'classnames';
-import * as React from 'react';
-import { ConfigContext } from '../config-provider';
-import useStyle from './style/index';
+import LoadingOutlined from '@ant-design/icons/LoadingOutlined';
+import CSSMotion from 'rc-motion';
+import React from 'react';
 
-export interface ActivityIndicatorProps {
-  prefixCls?: string;
-  size?: number;
-  color?: string;
-  loading: boolean;
+export interface LoadingIconProps {
+  prefixCls: string;
+  existIcon: boolean;
+  loading?: boolean | object;
+  loadingIconColor?: string;
+  size?: number | string;
 }
+const getCollapsedWidth = () => ({ width: 0, opacity: 0, transform: 'scale(0)' });
+const getRealWidth = (node: HTMLElement) => ({
+  width: node.scrollWidth,
+  opacity: 1,
+  transform: 'scale(1)',
+});
 
-const ActivityIndicator: React.FC<ActivityIndicatorProps> = ({
-  prefixCls: customizePrefixCls,
-  size,
-  color = '#888',
+const LoadingIcon: React.FC<LoadingIconProps> = ({
+  prefixCls,
   loading,
+  existIcon,
+  loadingIconColor,
+  size,
 }) => {
-  const { getPrefixCls } = React.useContext(ConfigContext);
+  const visible = !!loading;
 
-  const prefixCls = getPrefixCls('activity-indicator', customizePrefixCls);
+  if (existIcon) {
+    return (
+      <span className={`${prefixCls}-loading-icon`} style={{ color: loadingIconColor }}>
+        <LoadingOutlined color={loadingIconColor} style={{ fontSize: size }} />
+      </span>
+    );
+  }
 
-  const [wrapSSR, hashId] = useStyle(prefixCls);
-  const className = classNames(prefixCls, hashId);
-
-  return wrapSSR(
-    <div className={className} style={{ width: size, height: size }}>
-      {loading && (
-        <svg viewBox='0 0 100 100' xmlns='http://www.w3.org/2000/svg'>
-          <g transform='rotate(0 50 50)'>
-            <rect fill={color} height='16' rx='4' ry='4' width='8' x='46' y='22'>
-              <animate
-                attributeName='opacity'
-                begin='-0.546875s'
-                dur='0.625s'
-                keyTimes='0;1'
-                repeatCount='indefinite'
-                values='1;0'
-              />
-            </rect>
-          </g>
-          <g transform='rotate(45 50 50)'>
-            <rect fill={color} height='16' rx='4' ry='4' width='8' x='46' y='22'>
-              <animate
-                attributeName='opacity'
-                begin='-0.46875s'
-                dur='0.625s'
-                keyTimes='0;1'
-                repeatCount='indefinite'
-                values='1;0'
-              />
-            </rect>
-          </g>
-          <g transform='rotate(90 50 50)'>
-            <rect fill={color} height='16' rx='4' ry='4' width='8' x='46' y='22'>
-              <animate
-                attributeName='opacity'
-                begin='-0.390625s'
-                dur='0.625s'
-                keyTimes='0;1'
-                repeatCount='indefinite'
-                values='1;0'
-              />
-            </rect>
-          </g>
-          <g transform='rotate(135 50 50)'>
-            <rect fill={color} height='16' rx='4' ry='4' width='8' x='46' y='22'>
-              <animate
-                attributeName='opacity'
-                begin='-0.3125s'
-                dur='0.625s'
-                keyTimes='0;1'
-                repeatCount='indefinite'
-                values='1;0'
-              />
-            </rect>
-          </g>
-          <g transform='rotate(180 50 50)'>
-            <rect fill={color} height='16' rx='4' ry='4' width='8' x='46' y='22'>
-              <animate
-                attributeName='opacity'
-                begin='-0.234375s'
-                dur='0.625s'
-                keyTimes='0;1'
-                repeatCount='indefinite'
-                values='1;0'
-              />
-            </rect>
-          </g>
-          <g transform='rotate(225 50 50)'>
-            <rect fill={color} height='16' rx='4' ry='4' width='8' x='46' y='22'>
-              <animate
-                attributeName='opacity'
-                begin='-0.15625s'
-                dur='0.625s'
-                keyTimes='0;1'
-                repeatCount='indefinite'
-                values='1;0'
-              />
-            </rect>
-          </g>
-          <g transform='rotate(270 50 50)'>
-            <rect fill={color} height='16' rx='4' ry='4' width='8' x='46' y='22'>
-              <animate
-                attributeName='opacity'
-                begin='-0.078125s'
-                dur='0.625s'
-                keyTimes='0;1'
-                repeatCount='indefinite'
-                values='1;0'
-              />
-            </rect>
-          </g>
-          <g transform='rotate(315 50 50)'>
-            <rect fill={color} height='16' rx='4' ry='4' width='8' x='46' y='22'>
-              <animate
-                attributeName='opacity'
-                begin='0s'
-                dur='0.625s'
-                keyTimes='0;1'
-                repeatCount='indefinite'
-                values='1;0'
-              />
-            </rect>
-          </g>
-        </svg>
+  return (
+    <CSSMotion
+      visible={visible}
+      // We do not really use this motionName
+      motionName={`${prefixCls}-loading-icon-motion`}
+      removeOnLeave
+      onAppearStart={getCollapsedWidth}
+      onAppearActive={getRealWidth}
+      onEnterStart={getCollapsedWidth}
+      onEnterActive={getRealWidth}
+      onLeaveStart={getRealWidth}
+      onLeaveActive={getCollapsedWidth}
+    >
+      {({ className, style }: { className?: string; style?: React.CSSProperties }, ref: any) => (
+        <span className={`${prefixCls}-loading-icon`} style={style} ref={ref}>
+          <LoadingOutlined
+            className={className}
+            style={{ fontSize: size }}
+            color={loadingIconColor}
+          />
+        </span>
       )}
-    </div>,
+    </CSSMotion>
   );
 };
 
-export default ActivityIndicator;
+export default LoadingIcon;
