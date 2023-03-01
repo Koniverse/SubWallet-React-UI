f23e047a4e2b1bc625c1fb7878f72a6ba35589a1
AnhMTV <maithachvietanh@gmail.com>
2023-03-01 16:55:29 +0700

    Update default left logo

--- components/sw-header/icons/MenuBar.tsx
+++ components/sw-header/icons/MenuBar.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
 const MenuBar = () => (
-  <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
-      d="M5 8H27"
+      d="M4 6H20"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M5 16H27"
+      d="M4 12H20"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
     <path
-      d="M5 24H18"
+      d="M4 18H14"
       stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
