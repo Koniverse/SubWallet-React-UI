30ee08a65e03fce83ffb1ef81dc44f177e8358f6
AnhMTV <maithachvietanh@gmail.com>
2023-03-02 18:59:04 +0700

    Extend Color Palettes types

--- components/theme/interface/presetColors.ts
+++ components/theme/interface/presetColors.ts
@@ -33,4 +33,6 @@ type ColorPaletteKeyIndex = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
 export type ColorPalettes = {
   [key in `${keyof PresetColorType}-${ColorPaletteKeyIndex}`]: string;
+} & {
+  [key in `${keyof ThemePresetColorType}-${ColorPaletteKeyIndex}`]: string;
 };
